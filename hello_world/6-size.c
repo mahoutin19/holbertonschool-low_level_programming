0aea12a7fc2dae687680bcf452a91b736f36dc9b
root <root@Fulmine-10.localdomain>
2024-02-20 13:17:12 +0100

    formules

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -18,5 +18,5 @@ printf("Size of an int %lu byte(s)\n", sizeof(b));
 printf("Size of a long int %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int %lu byte(s)\n", sizeof(d));
 printf("Size of a float  %lu byte(s)\n", sizeof(f));
-Return (0);
+return (0);
 }
