b786e571be28e39b0557e76402cc69f71c734534
root <root@Fulmine-10.localdomain>
2024-02-20 13:55:55 +0100

    Les formules

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-char char_type;
-int int_type;
-long int long_int_type;
-long long int long_long_int_type;
-float float_type;
+	char char_type;
+	int int_type;
+	long int long_int_type;
+	long long int long_long_int_type;
+	float float_type;
 printf("Size of a char: %ld byte(s)\n", sizeof(char_type));
 printf("Size of an int: %ld byte(s)\n", sizeof(int_type));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long_int_type));
