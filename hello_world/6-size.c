87ece0b30bf0a3f95b05f39cff4a5cbc1f84a738
root <root@Fulmine-10.localdomain>
2024-02-20 13:07:46 +0100

    formules

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,14 +5,18 @@
  * primodiaux en c
  * Return: Always 0
  */
-
 int main(void)
-
 {
-	print("Size of a char %zu byte(s)\n", sizeof(char));
-	print("Size of an int %zu byte(s)\n", sizeof(int));
-	print("Size of a long int %zu byte(s)\n", sizeof(long int));
-	print("Size of a long long int %zu byte(s)\n", sizeof(long long int));
-	print("Size of a float  %zu byte(s)\n", sizeof(float));
-	Return 0;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char %lu byte(s)\n", (sizeof(a));
+	printf("Size of an int %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int %lu byte(s)\n", sizeof(d));
+	printf("Size of a float  %lu byte(s)\n", sizeof(f));
+	Return (0);
 }
