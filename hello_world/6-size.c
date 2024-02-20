4e8e8e761e14ea6360749f5adeacd38a8436de2c
root <root@Fulmine-10.localdomain>
2024-02-20 13:33:59 +0100

    formules

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,22 +1,19 @@
-#include <stdio.h>
-
 /**
- * main - prints les formules
- * primodiaux en c
- * Return: Always 0
+ * main - Entry
+ * show sizes
+ * Return: 0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("Size of a char %lu byte(s)\n", (sizeof(a));
-printf("Size of an int %lu byte(s)\n", sizeof(b));
-printf("Size of a long int %lu byte(s)\n", sizeof(c));
-printf("Size of a long long int %lu byte(s)\n", sizeof(d));
-printf("Size of a float  %lu byte(s)\n", sizeof(f));
+char char_type;
+int int_type;
+long int long_int_type;
+long long int long_long_int_type;
+float float_type;
+printf("Size of a char: %ld byte(s)\n", sizeof(char_type));
+printf("Size of an int: %ld byte(s)\n", sizeof(int_type));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long_int_type));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long_long_int_type));
+printf("Size of a float: %ld byte(s)\n", sizeof(float_type));
 return (0);
-}
+}}
