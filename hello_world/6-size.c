396d536779f023b0fcb8224083598f3f72192adf
root <root@Fulmine-10.localdomain>
2024-02-20 12:29:58 +0100

    formule

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types a based on the computer it is compiled an run on..
- * Return: Always 0.
+ * main - prints les formules
+ * primodiaux en c
+ * Return: Always 0
  */
 
 int main(void)
 
 {
-        printf("Size of a char: %zu byte(s)\n", sizeof(char));
-
-	printf("Size of an int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int))
-	printf("Size of a float int: %zu byte(s)\n", sizeof(float int));
-        return (0);
+	print("Size of a char %zu byte(s)\n", sizeof(char));
+	print("Size of an int %zu byte(s)\n", sizeof(int));
+	print("Size of a long int %zu byte(s)\n", sizeof(long int));
+	print("Size of a long long int %zu byte(s)\n", sizeof(long long int));
+	print("Size of a float  %zu byte(s)\n", sizeof(float));
+	Return 0;
 }
