c7ccf71d6b9c51752c389b6b8ae92b2641c4c1f1
root <root@Fulmine-10.localdomain>
2024-02-21 23:31:55 +0100

    Liste les alphabete

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: alaways (0)
+ */
+
+int main(void)
+{
+	char i
+		;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
+
