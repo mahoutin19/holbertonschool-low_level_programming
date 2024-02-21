f01b381eba4ad272524777669c11f457b063252e
root <root@Fulmine-10.localdomain>
2024-02-21 10:34:21 +0100

    If clauses

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    srand(time(NULL)); // Seed the random number generator
-    int n = rand() % (RAND_MAX + 1); // Generate a random number
-    printf("The number %d is ", n);
-    
-    if (n > 0) {
+
+
+
+int main(void)
+{
+	srand(time(NULL));
+	int n = rand() % (RAND_MAX + 1);
+
+	printf("The number %d is ", n);
+	if (n > 0) 
+	{
         printf("positive.\n");
-    } else if (n == 0) {
+	}
+       	else if (n == 0)
+       	{
         printf("zero.\n");
-    } else {
+	} 
+	else
+       	{
         printf("negative.\n");
-    }
-    
-    return 0;
+	}
+
+       	return (0);
 }
