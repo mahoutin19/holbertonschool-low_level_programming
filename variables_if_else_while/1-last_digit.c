c494d5181f37d50778254dcdf963e927a5c1c395
root <root@Fulmine-10.localdomain>
2024-02-21 20:59:57 +0100

    génère un nombre aléatoire n, l'imprime avec son dernier chiffre, puis imprime si le dernier chiffre est supérieur à 5, égal à 0 ou inférieur à 6 et non zero

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,20 +7,29 @@
  *
  * Return: 0
  */
-int main(void)
+
+iint main(void)
 {
 	int n;
 
-	rand(time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int last_digit = n % 10;
+	printf("Last digit of %d is %d and is ", n, n % 10);
 
-	if (last_digit > 5)
-		printf("%d and is greater than 5\n", last_digit);
-	else if (last_digit == 0)
-		printf("%d and is 0\n", last_digit);
+	if (n % 10 > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("0\n");
+	}
 	else
-		printf("%d and is less than 6 and not 0\n", last_digit);
+	{
+		printfprintf("less than 6 and not 0\n");
+	}
+
 	return (0);
+
 }
