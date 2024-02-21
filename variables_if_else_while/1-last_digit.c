b92b277df50abe2a5e9d14838d0bb127ff7b1594
root <root@Fulmine-10.localdomain>
2024-02-21 16:31:00 +0100

    Aléatoire

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,34 +1,22 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+#include <time.h>
+/* more headers goes there */
 
-/**
- * main - Prints a text according number
- *
- * Return: Always (Success)
- *
- */
-
+/* betty style doc for function main goes there */
 int main(void)
-
 {
-		int n, lastd;
+	int n;
 
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		lastd = n % 10;
-
-		if (lastd > 5)
-		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
-		}
-		else if (lastd == 0)
-		{
-			printf("Last digit of %d is %d and  0\n", n, lastd);
-		{
-		else if (lastd < 6 && lastd != 0)
-		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-		}
-		return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+       
+	int last_digit = n % 10;
+       
+	if (last_digit > 5)
+		printf("%d and is greater than 5\n", last_digit);
+	else if (last_digit == 0)
+		printf("%d and is 0\n", last_digit);
+	else
+		printf("%d and is less than 6 and not 0\n", last_digit);
+	return (0);
 }
