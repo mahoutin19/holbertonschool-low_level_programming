78f38545946a00b0407f5805158fa1e0a93217b0
root <root@Fulmine-10.localdomain>
2024-02-22 10:48:26 +0100

    Affiche les chiffres dans un intervalle donné

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: alaways (0)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
