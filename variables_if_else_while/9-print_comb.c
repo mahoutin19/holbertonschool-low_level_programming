bc4515a27cc824f6ea11e83438af6764e15ffa15
root <root@Fulmine-10.localdomain>
2024-02-23 11:09:50 +0100

    Conbinaison de chiffre avec espace

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: alaways (0)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+
+}	
