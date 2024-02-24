49d95fa2d62bffc3662ba6e742ba2a8943d50de2
root <root@Fulmine-10.localdomain>
2024-02-24 12:23:00 +0100

    lettre alpha en minuscule

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - main function
+ *
+ * Return: alaways (0)
+ */
+
+void print_alphabet(void)
+{
+	char ltr;
+	for (ltr = 'a'; ltr <= 'z'; ltr++)
+	{
+		_putchar(ltr);
+	}
+	_putchar('\n');
+}
+
