d63473e6d2e484e9ad54695685e35febb6e8cc7d
root <root@Fulmine-10.localdomain>
2024-02-25 01:52:20 +0100

    Calcule la valeur absolue

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolue value of an integer.
+ * @n: The number to be computed.
+ * Return: Absolute value of number or 0
+ */
+
+int _abs(int n)
+
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+
+}
