ee21fd6f4017befacb2e2be6a2acc4fc624a1446
root <root@Fulmine-10.localdomain>
2024-02-27 10:13:36 +0100

    Affiche des nombre sauf un chiffre donné

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,25 +2,20 @@
 
 /**
  * print_most_numbers - Print the numbers since 0 up to 9
- * Description: Prints the numbers excluding 2 and '
+ * Description: Prints the numbers excluding 2 and 4
  * Return: The numbers since 0 up to 9
  */
 
 void print_most_numbers(void)
 {
-	int c = 0;
-
-	for (; c <= 9; c++)
-	{
-		if (c == 2 || c == 4)
-		{
-			continue;
-		}
-		else
-		{
-			
-			_putchar(c + '0');
-		}
-	}
-	_putchar('\n');
+int nbr;
+for (nbr = 0; nbr < 9; nbr++)
+{
+if ((nbr == 2) || (nbr == 4))
+{
+continue;
+}
+_putchar(nbr);
+}
+_putchar('\n');
 }
