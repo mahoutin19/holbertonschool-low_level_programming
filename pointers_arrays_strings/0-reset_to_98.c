e296928b6e551d98eb6d94e9fa7710d0e58076f9
root <root@Fulmine-10.localdomain>
2024-02-28 16:40:20 +0100

    Fonction qui prend un pointeur vers un int comme parametre

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Takes a pointers to an int updates
+ * the value it points to to 98
+ *
+ * @n: An intefer input pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
