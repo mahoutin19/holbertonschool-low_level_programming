af9b0f852551c6f5f3bc6e1b1566aad2751d4009
root <root@Fulmine-10.localdomain>
2024-03-05 16:42:25 +0100

    Affiche deuc somme diagonales d'une matrice carrée

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,24 +1,23 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
-* print_diagsums - sum of the two diagonals
-* @a: array
-* @size: size
-*/
+ * print_diagsums - Print diagonal sum.
+ * @a: an array of pointers of integers
+ * @size: array size
+ */
 void print_diagsums(int *a, int size)
 {
-	int b1 = 0;
-	int b2 = 0;
-	int index = 0;
+	int i, j, k, d1, d2;
 
-	while (index < size)
-	{
-		b1 += a[index * (size + 1)];
-		b2 += a[(index + 1) * (size - 1)];
-		index++;
-	}
-	printf("%d, ", b1);
-	printf("%d", b2);
-	printf("\b");
+	k = 0;
+	d1 = 0;
+	d2 = 0;
+	for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
+		{
+			(i == j) ? d1 += a[k] : 0;
+			(i + j == size - 1) ? d2 += a[k] : 0;
+			k++;
+		}
+	printf("%i, %i\n", d1, d2);
 }
