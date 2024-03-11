4d225669470a60913c730bcfa1d475c5f1b8a266
root <root@Fulmine-10.localdomain>
2024-03-11 13:12:44 +0100

    Envoie un pointeur vers une nouvelle alocation de memoire d'un chaine copiee

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * char *_strdup - duplicaced a string
+ * @str: the string to duplicate
+ * Return : the string duplicated
+ */
+
+char *_strdup(char *str)
+{
+	int a = 0, index = 1;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[index])
+	{
+		index++;
+	}
+
+	s = malloc((sizeof(char) * index) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	while (a < index)
+	{
+		s[a] = str[a];
+		a++;
+	}
+
+	s[a] = '\0';
+	return (s);
+}
