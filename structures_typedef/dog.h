0361d0c78d58aaf15fa70a9d41026c1012921d4c
mahoutin19 <fulmineokan8@gmail.com>
2024-03-14 13:25:11 +0000

    Definis une structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,25 +1,23 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - dog data
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner of the dog
-
- * Description: Defines metadata about the dog and type of dog's struct
- */
+*struct dog - Dog attributes
+*@name: The name of the dog
+*@age: The age of the dog
+*@owner: The owner of the dog
+*Description: The attributes of a dog.
+*/
 
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
-} dog_t;
+}dog_t;
 
 /**
- * Additional function prototypes 
+*Additional function prototypes
 */
 
 int _putchar(char c);
@@ -27,4 +25,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif;
+#endif
