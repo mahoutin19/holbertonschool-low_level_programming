4958154c6fbfcdddc8d0e0bdc042a0ae7a8877c8
root <root@Fulmine-10>
2024-03-15 13:39:34 +0100

    Creer un nouveau espece

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,38 +1,47 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
-  * new_dog - creates a new struct of type dog
-  * @name: struct parameter name
-  * @age: struct parameter age
-  * @owner: struct parameter owner
-  * Return: returns pointer to buffer of datatype dog
-  */
+ * new_dog - creates a new struct of type dog
+ * @name: struct parameter name
+ * @age: struct parameter age
+ * @owner: struct parameter owner
+ * Return: returns pointer to buffer of datatype dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int n_len, o_len, index;
-	dog_t *doggy;
+int n_len = 0, o_len = 0, index;
+dog_t *doggy;
 
-	n_len = o_len = 0;
-	while (name[n_len++])
-		;
-	while (owner[o_len++])
-		;
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
-		return (NULL);
+while (name[n_len++])
+while (owner[o_len++])
+doggy = malloc(sizeof(dog_t));
 
-	doggy->name = malloc(n_len * sizeof(doggy->name));
-	if (doggy == NULL)
-		return (NULL);
-	for (index = 0; index < n_len; index++)
-		doggy->name[index] = name[index];
+if (doggy == NULL)
+return (NULL);
 
-	doggy->age = age;
+doggy->name = malloc(n_len * sizeof(char));
+if (doggy->name == NULL)
+{
+free(doggy);
+return (NULL);
+}
+for (index = 0; index < n_len - 1; index++)
+doggy->name[index] = name[index];
+doggy->name[index] = '\0';
 
-	doggy->owner = malloc(o_len * sizeof(doggy->owner));
-	if (doggy == NULL)
-		return (NULL);
-	for (index = 0; index < o_len; index++)
-		doggy->owner[index] = owner[index];
-	return (doggy);
+doggy->age = age;
+
+doggy->owner = malloc(o_len * sizeof(char));
+if (doggy->owner == NULL)
+{
+free(doggy->name);
+free(doggy);
+return (NULL);
+}
+for (index = 0; index < o_len - 1; index++)
+doggy->owner[index] = owner[index];
+doggy->owner[index] = '\0';
+
+return (doggy);
 }
