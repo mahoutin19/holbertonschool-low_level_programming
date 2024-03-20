cb36f1cf0222a5e708518c831b6b0e3a25b323d5
root <root@Fulmine-10>
2024-03-20 12:10:07 +0100

    Affiche le nom

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
 *print_name - Prints a name
 *@name: The name to ptints
@@ -8,7 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL && f != NULL)
-    f(name);
-
+if (name != NULL && f != NULL)
+f(name);
 }
